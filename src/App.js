2d2db39d891bbca4f3e62326cf199df4a84fb3c0
Alex Silva <alex.silva.05@outlook.com>
2022-10-24 20:33:06 -0300

    desenvolvimento

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import './App.css';
 import Evento from './components/Evento';
-import FormUser from './components/FormUser';
 
 function App() {
 
@@ -9,7 +8,6 @@ function App() {
     <div className="App">
     <h1>Testando evento</h1>
       <Evento />
-      <FormUser/>
     </div>
   );
 
--- src/components/Evento.js
+++ src/components/Evento.js
@@ -1,17 +1,24 @@
+import Button from './evento/Button'
+
 function Evento(){
     return(
         <>
         <p>Clique para realizar o evento:</p>
-        
-        <button onClick={meuEvento}>Clique aqui</button>
-
+        <Button event={meuEvento} text="Primeiro Evento"></Button>
+        <Button event={segundoEvento} text="Segundo Evento"></Button>
         </>
     )
+
+function segundoEvento(){
+    console.log('Segundo evento disparado')
+    alert('Segundo evento disparado')
 }
 
+
 function meuEvento(){
-    console.log('Evento realizado com sucesso!')
-    alert('Evento disparado')
+    console.log('Ativando Primeiro Evento')
+    alert('Ativando Primeiro Evento')
+}
 }
 
 export default Evento
